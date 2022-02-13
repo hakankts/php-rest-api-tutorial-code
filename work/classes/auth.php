b07304f9c88851636a457637be61ef62a6b30f4b
hakan karataş <hakan_kts@yahoo.com>
2022-02-13 11:36:44 +0300

    Update auth.php

--- work/classes/auth.php
+++ work/classes/auth.php
@@ -5,7 +5,7 @@ class auth {
 
 	function auth_f($user_name,$password){
  		
-		$api_url = "http://detaytrafik.com/ws/api/api_actions.php?action=auth&user_name=$user_name&password=$password";
+		$api_url = "http://localhost/ws/api/api_actions.php?action=auth&user_name=$user_name&password=$password";
 		$client = curl_init($api_url);
 		curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 		$response = curl_exec($client);		
